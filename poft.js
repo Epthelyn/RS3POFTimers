32bb5aa4d2bb08e66febca3aedb8fa6f2d170c95
Epthelyn <simon.d.fiander@gmail.com>
2022-02-27 12:57:09 +0000

    Animal amounts
    - General cleanup of duplicated code
    - Added animal amount field
    - Days/Hours display now also shows an estimate of food remaining

--- poft.js
+++ poft.js
@@ -49,6 +49,32 @@ const _pof = function(){
             saveValuesToLocalStorage();
         });
 
+        $('.penAnimalAmount').on('change', function(){
+            const parent = $(this).parent();
+            const penType = $(parent).attr('penType');
+
+            const location = penType.split("_")[0];
+            const pen = penType.split("_")[1];
+
+            let quantity = parseInt($(this).val());
+            if(isNaN(quantity)){
+                $(this).val(0);
+                alert("Animal amount must be a number!");
+            }
+            else if(quantity < 0){
+                $(this).val(0);
+                alert("Animal amount must be non-negative!");
+            }
+            else{
+                timers[location][pen].animalCount = quantity;
+                timers[location][pen].time = Date.now();
+
+                saveValuesToLocalStorage();
+
+                updateFoodTime(location,pen);
+            }
+        });
+
         $('.penFoodAmount').on('change', function(){
             const parent = $(this).parent();
             const penType = $(parent).attr('penType');
@@ -68,41 +94,59 @@ const _pof = function(){
                 $(this).val(0);
                 alert("Food amount must be non-negative!");
             }
-            else if(quantity == 0){
-                timers[location][pen].food = 0;
-                timers[location][pen].time = Date.now();
-                const timeCell = $(parent).find('.penFoodOutTimes');
-                $(timeCell).html("NO FOOD!");
-                $(timeCell).addClass('bad');
-                $(timeCell).removeClass('good');
-                $(timeCell).removeClass('soon');
-
-                saveValuesToLocalStorage();
-            }
             else{
                 timers[location][pen].food = quantity;
                 timers[location][pen].time = Date.now();
 
-                const timeCell = $(parent).find('.penFoodOutTimes');
-                const expiryTime = Date.now() + ONE_HOUR*(1+Math.floor(quantity/animalAmounts[pen]));
-                const expiryHours = ONE_HOUR*(1+Math.floor(quantity/animalAmounts[pen]))/(1000*3600);
-    
-                $(timeCell).html(`${~~(expiryHours/24)}D ${~~expiryHours%24}H`);
-
-                if(expiryTime - Date.now() < ONE_DAY){
-                    $(timeCell).addClass('soon');
-                    $(timeCell).removeClass('good');
-                    $(timeCell).removeClass('bad');
-                }
-                else{
-                    $(timeCell).addClass('good');
-                    $(timeCell).removeClass('bad');
-                    $(timeCell).removeClass('soon');
-                }
-
                 saveValuesToLocalStorage();
+
+                updateFoodTime(location,pen);
             }
         });
+    }
+
+    const updateFoodTime = (location, pen) => {
+
+        const quantity = timers[location][pen].food;
+        const time = timers[location][pen].time;
+        const animalCount = timers[location][pen].animalCount;
+
+        const penRow = $(`.penRow[penType="${location}_${pen}"]`);
+        const timeCell = $(penRow).find('.penFoodOutTimes');
+
+        const expiryTime = time + ONE_HOUR*(1+Math.floor(quantity/animalCount));
+        const timeDiff = Math.max(0,expiryTime-Date.now());
+        const timeElapsed = Date.now() - time;
+        const hoursElapsed = timeElapsed/ONE_HOUR;
+
+        const foodRemain = ~~(quantity - animalCount*hoursElapsed);
+
+        const expiryHours = timeDiff/ONE_HOUR;
+
+
+        if(quantity > 0){
+
+            $(timeCell).html(`${~~(expiryHours/24)}D ${~~expiryHours%24}H (${foodRemain})`);
+            if(expiryTime - Date.now() < ONE_DAY){
+                $(timeCell).addClass('soon');
+                $(timeCell).removeClass('good');
+                $(timeCell).removeClass('bad');
+            }
+            else{
+                $(timeCell).addClass('good');
+                $(timeCell).removeClass('bad');
+                $(timeCell).removeClass('soon');
+            }
+        }
+        else{
+            $(timeCell).html("NO FOOD!");
+            $(timeCell).addClass('bad');
+            $(timeCell).removeClass('good');
+            $(timeCell).removeClass('soon');
+        }
+        
+
+
     }
 
     const loadTimersFromLocalStorage = () => {
@@ -115,43 +159,14 @@ const _pof = function(){
                     time: lsData["ManorFarm"][k].time || 0,
                     food: lsData["ManorFarm"][k].food || 0,
                     animal: lsData["ManorFarm"][k].animal || "empty",
+                    animalCount: lsData["ManorFarm"][k].animalCount || animalAmounts[k]
                 }
 
                 $(`.penRow[pentype="ManorFarm_${k}"] > .penAnimals`).val(timers["ManorFarm"][k].animal);
                 $(`.penRow[pentype="ManorFarm_${k}"] > .penFoodAmount`).val(timers["ManorFarm"][k].food);
+                $(`.penRow[pentype="ManorFarm_${k}"] > .penAnimalAmount`).val(timers["ManorFarm"][k].animalCount);
 
-                const expiryTime = timers["ManorFarm"][k].time + ONE_HOUR*(1+Math.floor(timers["ManorFarm"][k].food/animalAmounts[k]));
-                const expiryHours = (timers["ManorFarm"][k].time + ONE_HOUR*(1+Math.floor(timers["ManorFarm"][k].food/animalAmounts[k])) - Date.now())/(1000*3600);
-
-                const timeCell =  $(`.penRow[pentype="ManorFarm_${k}"] > .penFoodOutTimes`);
-
-
-                
-                if(Date.now() > expiryTime){
-                    $(timeCell).removeClass('soon');
-                    $(timeCell).removeClass('good');
-                    $(timeCell).addClass('bad');
-                }
-                else if(expiryTime - Date.now() < ONE_DAY){
-                    $(timeCell).addClass('soon');
-                    $(timeCell).removeClass('good');
-                    $(timeCell).removeClass('bad');
-                }
-                else{
-                    $(timeCell).addClass('good');
-                    $(timeCell).removeClass('bad');
-                    $(timeCell).removeClass('soon');
-                }
-
-                if(timers["ManorFarm"][k].food > 0){
-                    $(timeCell).html(`${~~(expiryHours/24)}D ${~~expiryHours%24}H`);
-                }
-                else{
-                    $(timeCell).html("NO FOOD!");
-                    $(timeCell).removeClass('soon');
-                    $(timeCell).removeClass('good');
-                    $(timeCell).addClass('bad');
-                }
+                updateFoodTime("ManorFarm",k);
             }
 
             for(k in lsData["Anachronia"]){
@@ -159,43 +174,14 @@ const _pof = function(){
                     time: lsData["Anachronia"][k].time || 0,
                     food: lsData["Anachronia"][k].food || 0,
                     animal: lsData["Anachronia"][k].animal || "empty",
+                    animalCount: lsData["Anachronia"][k].animalCount || animalAmounts[k]
                 }
 
                 $(`.penRow[pentype="Anachronia_${k}"] > .penAnimals`).val(timers["Anachronia"][k].animal);
                 $(`.penRow[pentype="Anachronia_${k}"] > .penFoodAmount`).val(timers["Anachronia"][k].food);
+                $(`.penRow[pentype="Anachronia_${k}"] > .penAnimalAmount`).val(timers["Anachronia"][k].animalCount);
 
-                const expiryTime = timers["Anachronia"][k].time + ONE_HOUR*(1+Math.floor(timers["Anachronia"][k].food/animalAmounts[k]));
-                const expiryHours = (timers["Anachronia"][k].time + ONE_HOUR*(1+Math.floor(timers["Anachronia"][k].food/animalAmounts[k])) - Date.now())/(1000*3600);
-
-                const timeCell =  $(`.penRow[pentype="Anachronia_${k}"] > .penFoodOutTimes`);
-
-
-                
-                if(Date.now() > expiryTime){
-                    $(timeCell).removeClass('soon');
-                    $(timeCell).removeClass('good');
-                    $(timeCell).addClass('bad');
-                }
-                else if(expiryTime - Date.now() < ONE_DAY){
-                    $(timeCell).addClass('soon');
-                    $(timeCell).removeClass('good');
-                    $(timeCell).removeClass('bad');
-                }
-                else{
-                    $(timeCell).addClass('good');
-                    $(timeCell).removeClass('bad');
-                    $(timeCell).removeClass('soon');
-                }
-
-                if(timers["Anachronia"][k].food > 0){
-                    $(timeCell).html(`${~~(expiryHours/24)}D ${~~expiryHours%24}H`);
-                }
-                else{
-                    $(timeCell).html("NO FOOD!");
-                    $(timeCell).removeClass('soon');
-                    $(timeCell).removeClass('good');
-                    $(timeCell).addClass('bad');
-                }
+                updateFoodTime("Anachronia",k);
             }
         }
         else{
